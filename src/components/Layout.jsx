e865b61be3251fee5d46a71767be42071eb5a3d0
Redk0rd <red.kord95@gmail.com>
2024-02-08 17:52:58 +0300

    debag navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,11 +6,15 @@ export default function Layout({ children, initState }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin />
-
         <script
-          src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
-          crossOrigin
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+          integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+          crossOrigin="anonymous"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+          integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+          crossOrigin="anonymous"
         />
         <link
           rel="stylesheet"
