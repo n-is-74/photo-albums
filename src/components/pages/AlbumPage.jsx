ef784f16ae9d82de8ba2e1915d550fa78b042ba5
Alexander Toma <alextoma1994@gmail.com>
2024-02-09 15:03:25 +0300

    add photos router

--- src/components/pages/AlbumPage.jsx
+++ src/components/pages/AlbumPage.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import AlbumCard from '../ui/AlbumCard';
 
 export default function AlbumPage({ accessibleAlbums }) {
-  console.log(accessibleAlbums);
-
   return (
     <div>
       <div>
