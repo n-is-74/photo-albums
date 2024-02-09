baf4a2ddeb2c37394c9457b00361feb9fa2321c7
Redk0rd <red.kord95@gmail.com>
2024-02-09 11:02:16 +0300

    refack1.2

---
+++ db/seeders/20240209075753-All.js
@@ -0,0 +1,67 @@
+module.exports = {
+  async up(queryInterface) {
+    // Добавляем пользователей
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          name: 'Alice Smith',
+          email: 'alice@example.com',
+          password: 'alice123',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Bob Johnson',
+          email: 'bob@example.com',
+          password: 'bob123',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Charlie Brown',
+          email: 'charlie@example.com',
+          password: 'charlie123',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+    // Добавляем альбомы для каждого пользователя
+    const albums = [];
+    for (let userId = 1; userId <= 3; userId++) {
+      for (let i = 1; i <= 4; i++) {
+        albums.push({
+          a_name: `Album ${i} of User ${userId}`,
+          author_id: userId,
+          private: i % 2 === 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+      }
+    }
+    await queryInterface.bulkInsert('Albums', albums, {});
+    // Добавляем фотографии в каждый альбом
+    const photos = [];
+    let imageId = 1;
+    for (let albumId = 1; albumId <= albums.length; albumId++) {
+      for (let i = 1; i <= 5; i++) {
+        photos.push({
+          img: `https://rickandmortyapi.com/api/character/avatar/${imageId}.jpeg`,
+          title: `Image ${imageId}`,
+          album_id: albumId,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+        imageId++;
+      }
+    }
+    await queryInterface.bulkInsert('Photos', photos, {});
+  },
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Photos', null, {});
+    await queryInterface.bulkDelete('Albums', null, {});
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
